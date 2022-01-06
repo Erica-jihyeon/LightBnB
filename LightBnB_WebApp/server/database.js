f8a8884382dbf20597ad837e7ab0c85e664da389
Erica-jihyeon <jihyeon20@gmail.com>
2022-01-06 17:10:32 +0000

    add user's reservations list query

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -102,9 +102,29 @@ exports.addUser = addUser;
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
+// const getAllReservations = function(guest_id, limit = 10) {
+//   return getAllProperties(null, 2);
+// }
+
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const queryString = `SELECT properties.*, reservations.*, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  GROUP BY properties.id, reservations.id
+  HAVING reservations.guest_id=$1 AND reservations.end_date < now()::date
+  ORDER BY reservations.start_date
+  LIMIT $2;
+  `;
+  return pool
+    .query(queryString, [guest_id, limit])
+    .then((result) => result.rows)
+    .catch((err) => {
+      console.log(err.message);
+    });
 }
+
+
 exports.getAllReservations = getAllReservations;
 
 /// Properties
